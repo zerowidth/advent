355bce3bf3ec8a1585001efe2f49564abf14444d
Nathan Witmer <nathan@zerowidth.com>
2017-12-01 22:15:07 -0700

    Day 2 part 2

--- 2017/02.rb
+++ 2017/02.rb
@@ -1,7 +1,5 @@
-def go(sym, input, expected = nil)
-  print "#{input} => "
-  value = method(sym).call(input)
-  print "#{value}"
+def go(input, value, expected = nil)
+  print "#{input} => #{value}"
   if expected
     if value == expected
       puts " (OK)"
@@ -13,9 +11,9 @@ def go(sym, input, expected = nil)
   end
 end
 
-def checksum(input)
+def checksum(input, &block)
   rows = input.lines.map(&:strip).reject(&:empty?).map { |l| l.split(/\s+/).map(&:to_i) }
-  rows.map { |row| row.max - row.min }.sum
+  rows.map(&block).sum
 end
 
 s = <<-S
@@ -24,5 +22,25 @@ s = <<-S
 2 4 6 8
 S
 
-go :checksum, s, 18
-go :checksum, `pbpaste`
+# go(s, checksum(s) { |row| row.max - row.min }, 18)
+# go(`pbpaste`, checksum(`pbpaste`) { |row| row.max - row.min })
+
+t = <<-T
+5 9 2 8
+9 4 7 3
+3 8 6 5
+T
+
+x = checksum(`pbpaste`) do |row|
+  # evenly divides means mod is 0
+  sum = 0
+  row.each do |i|
+    row.each do |j|
+      if i > j && i % j == 0
+        sum += (i / j)
+      end
+    end
+  end
+  sum
+end
+puts x
