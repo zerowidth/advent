bbc9c62fb3a35858f79cb260d7ddafe061081e8e
Nathan Witmer <nathan@zerowidth.com>
2017-12-02 21:31:02 -0700

    2015 day 8 part 2

--- 2015/08.rb
+++ 2015/08.rb
@@ -10,12 +10,6 @@ def matchsticks(input)
   sum
 end
 
-def part_one(line)
-  code = line.length
-  data = count_chars(line[1..-2])
-  code - data
-end
-
 def count_chars(input)
   chars = input.chars
   count = 0
@@ -32,6 +26,29 @@ def count_chars(input)
   count
 end
 
+def encode(line)
+  final = ['"']
+  line.chars.each do |char|
+    case char
+    when '"', '\\'
+      final << '\\'
+    end
+    final << char
+  end
+  final << '"'
+  final.join
+end
+
+def part_one(line)
+  code = line.length
+  data = count_chars(line[1..-2])
+  code - data
+end
+
+def part_two(line)
+  encode(line).length - line.length
+end
+
 part 1
 with :matchsticks, &method(:part_one)
 try '""', 2
@@ -42,4 +59,9 @@ try '"\\\\"', 2
 try puzzle_input
 
 part 2
-puts "TBD"
+with :matchsticks, &method(:part_two)
+try '""', 4
+try '"abc"', 4
+try '"aaa\\"aaa"', 6
+try '"\x27"', 5
+try puzzle_input
