9df0f8e688dc31d64b4e949ce42d01d84245fe3f
Nathan Witmer <nathan@zerowidth.com>
2017-12-02 21:24:34 -0700

    2015 day 8 part 1

---
+++ 2015/08.rb
@@ -0,0 +1,45 @@
+require_relative "../toolkit"
+
+def matchsticks(input)
+  sum = 0
+  input.lines.map do |line|
+    line = line.gsub /\s/, ""
+    next if line.length == 0
+    sum += yield line
+  end
+  sum
+end
+
+def part_one(line)
+  code = line.length
+  data = count_chars(line[1..-2])
+  code - data
+end
+
+def count_chars(input)
+  chars = input.chars
+  count = 0
+  while char = chars.shift
+    if char == '\\'
+      if chars.first == '"' || chars.first == '\\'
+        chars.shift
+      elsif chars[0] == 'x'
+        3.times { chars.shift }
+      end
+    end
+    count += 1
+  end
+  count
+end
+
+part 1
+with :matchsticks, &method(:part_one)
+try '""', 2
+try '"abc"', 2
+try '"aaa\\"aaa"', 3
+try '"\x27"', 5
+try '"\\\\"', 2
+try puzzle_input
+
+part 2
+puts "TBD"
