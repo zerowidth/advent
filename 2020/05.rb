7f8edb5b565ebcd115d79b7125f748cb96cac74a
Nathan Witmer <nathan@zerowidth.com>
2020-12-07 11:54:29 -0700

    More golf on 2020 day 5

--- 2020/05.rb
+++ 2020/05.rb
@@ -9,14 +9,12 @@ ex3 = "FFFBBBFRRR"
 ex4 = "BBFFBBFRLL"
 
 def part1(input)
-  input.each_line.map(&:strip).map do |line|
-    line.gsub(/./, "B" => 1, "F" => 0, "R" => 1, "L" => 0).to_i(2)
-  end.sort.last
+  input.each_line.map { |line| line.strip.tr("BFRL", "1010").to_i(2) }.sort.last
 end
 
 def part2(input)
-  input.each_line.map(&:strip).map do |line|
-    line.gsub(/./, "B" => 1, "F" => 0, "R" => 1, "L" => 0).to_i(2)
+  input.each_line.map do |line|
+    line.strip.tr("BFRL", "1010").to_i(2)
   end.sort.each_cons(2).detect { |a, b| b - a > 1 }.first + 1
 end
 
