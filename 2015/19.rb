d7a8aef97fc5b475ca91faa97c6ff566f543651a
Nathan Witmer <nathan@zerowidth.com>
2021-11-10 23:05:27 -0700

    Got Earley parser working for 2015 day 19

--- 2015/19.rb
+++ 2015/19.rb
@@ -3,7 +3,7 @@ require_relative "../graph_search"
 
 Signal.trap("INT") { exit }
 
-ATOMS = /[eA-Z()][a-z]*/
+ATOMS = /[eA-Z()][a-z]*/ # the () are for testing the Earley parser
 
 ex1 = <<-EX
 H => HO
@@ -15,7 +15,7 @@ EX
 
 def part1(input)
   rules, input = input.split("\n\n", 2)
-  rules = rules.split("\n").each_with_object(Hash.new) do |rule, hash|
+  rules = rules.split("\n").each_with_object({}) do |rule, hash|
     k, v = rule.strip.split(" => ", 2)
     hash[k] ||= []
     hash[k] << v
@@ -28,7 +28,7 @@ def part1(input)
       matches.each do |rule|
         replacement = start.dup
         replacement[i] = rule
-        found << replacement.join("")
+        found << replacement.join
       end
     end
   end
@@ -36,20 +36,28 @@ def part1(input)
 end
 
 class Earley
-
   class Item
-    attr_reader :lhs, :rhs, :dot, :start, :pos
+    attr_reader :lhs, :rhs, :dot, :from, :to
 
     # lhs: the nonterminal on the left-hand side of a rule
     # rhs: an array of terminals and nonterminals on the right hand side of a production rule
     # dot: the integer position in the rhs where this item is located
-    # start: the integer starting position of this item
-    def initialize(lhs, rhs, dot, start, pos)
+    # from: the integer starting position of this item
+    # to: the integer position of the end of this item
+    def initialize(lhs, rhs, dot, from, to)
       @lhs = lhs
       @rhs = rhs
       @dot = dot
-      @start = start
-      @pos = pos # implcitly encoded in state_set[i], but make it explicit for later
+      @from = from
+      @to = to # implcitly encoded in state_set[i], but make it explicit for later
+    end
+
+    def length
+      to - from
+    end
+
+    def prev
+      dot.zero? ? nil : rhs[dot - 1]
     end
 
     def next
@@ -63,7 +71,7 @@ class Earley
     def to_s
       with_dot = rhs.dup
       with_dot.insert(dot, "•")
-      "#{lhs} -> #{with_dot.join(" ")} (#{start}) @ #{pos}"
+      "#{lhs} -> #{with_dot.join(" ")} (#{from}, #{to})"
     end
 
     def inspect
@@ -72,20 +80,34 @@ class Earley
 
     include Comparable
     def <=>(other)
-      o = other ? [other.lhs, other.rhs, other.dot, other.start, other.pos] : nil
-      [lhs, rhs, dot, start, pos] <=> o
+      o = other ? [other.lhs, other.rhs, other.dot, other.from, other.to] : nil
+      [lhs, rhs, dot, from, to] <=> o
+    end
+  end
+
+  class Terminal
+    def initialize(symbol, pos)
+      @symbol = symbol
+      @pos = pos
+    end
+
+    def from
+      @pos
+    end
+
+    def to_s
+      @symbol.to_s
+    end
+
+    def inspect
+      "<#{self.class.name}: #{self}>"
     end
   end
 
   attr_reader :rules
 
-  def initialize(rules, debug: false)
+  def initialize(rules)
     @rules = rules
-    @debug = debug
-  end
-
-  def debug(msg)
-    puts msg if @debug
   end
 
   def recognize(input, starting_production)
@@ -103,9 +125,9 @@ class Earley
         debug item
         if item.complete? # completion
           # debug "  completing #{item} at S(#{item.start})?"
-          state_set[item.start].each do |start_item|
+          state_set[item.from].each do |start_item|
             if start_item.next == item.lhs
-              completed = Item.new(start_item.lhs, start_item.rhs, start_item.dot + 1, start_item.start, pos)
+              completed = Item.new(start_item.lhs, start_item.rhs, start_item.dot + 1, start_item.from, pos)
               unless set.include?(completed)
                 debug "  complete: #{item} : #{start_item} => #{completed}"
                 set << completed
@@ -116,8 +138,10 @@ class Earley
           next
         end
 
-        # this ignores a check for whether the next token is nonterminal, just
-        # look up an expansion rule either way and see if we can predict any
+        # This ignores a check for whether the next token is nonterminal, just
+        # look up an expansion rule either way and see if we can predict any. In
+        # our grammars, there's no distinction between terminals and
+        # nonterminals.
         rules.fetch(item.next, []).each do |rhs|
           next_item = Item.new(item.next, rhs, 0, pos, pos)
           skip = set.include?(next_item)
@@ -128,7 +152,7 @@ class Earley
         # similarly: ignore if the next token is nonterminal, we only care if
         # it appears in the input stream
         if input[pos] == item.next
-          next_item = Item.new(item.lhs, item.rhs, item.dot + 1, item.start, pos + 1)
+          next_item = Item.new(item.lhs, item.rhs, item.dot + 1, item.from, pos + 1)
           skip = state_set[pos + 1].include?(next_item)
           debug "  scan: #{item.next} => #{next_item}#{" (skip)" if skip}" unless skip
           state_set[pos + 1] << next_item unless skip
@@ -144,69 +168,75 @@ class Earley
   def parse(input, starting_production)
     state_set = recognize(input, starting_production)
 
-    debug "\nbuilding parse tree for #{input.map(&:to_s).join} from:"
-    state_set.each.with_index do |set, i|
-      next unless set && !set.empty?
-      puts "=== #{i} ==="
-      puts set.map(&:to_s).join("\n")
-      puts
+    recognized = state_set.last.select { |item| item.complete? && item.from.zero? && item.lhs == starting_production }
+    return nil if recognized.empty?
+
+    puts "recognized! #{recognized}"
+    recognized = recognized.first # FIXME: make this a loop again
+
+    by_start = state_set.flatten.group_by(&:from)
+    debug "\nbuilding parse tree from:"
+    by_start.each do |i, set|
+      debug "=== #{i} ==="
+      debug set.map(&:to_s).join("\n")
+      debug
     end
 
-    # completed state set for:
-    # e => H
-    # e => O
-    # H => HO
-    # H => OH
-    # O => HH
-    #
-    # H O H
-    #
-    # === 0 ===
-    #
-    # === 1 ===
-    # e -> H • (0) @ 1
-    #
-    # === 2 ===
-    # H -> H O • (0) @ 2
-    # e -> H • (0) @ 2
-    #
-    # === 3 ===
-    # H -> O H • (1) @ 3
-    # O -> H H • (0) @ 3
-    # e -> O • (0) @ 3
+    build_tree(recognized, input, by_start)
+  end
 
-    # starting with the `e` matching the whole input:
-    #   e -> O • (0) @ 3
-    # find an O starting at 0 ending at 3: O -> H H • (0)
-    #  * find an H starting at 0:
-    #    * the terminal H starts at 0
-    #      * now, find an H starting at 1 ending at 3: H -> O H • (1) @ 3
-    #        -> path: e -> O, O -> H H, H -> O H
-    #    * the rule H -> H O • (0) @ 2
-    #      * the terminal H starts at 0
-    #        * find an O starting at 1, ending at 2
-    #      * find an H starting at 0:
-    #        * the rule H -> H O • (0) @ 2 (this rule): find an O starting at 3, ending at 2? no.
-    #    * find an H starting at 0:
-    #
+  def build_tree(item, input, states, tree = [item])
+    parts = decompose(item, input, states, 0, item.from)
+    tree + parts.map { |part| build_tree(part, input, states, [part]) }
+  end
 
-    paths = []
-    state_set.last.select { |item| item.complete? && item.start.zero? && item.lhs == "e" }.each do |top|
-      path = []
-      stack = [top, 0] # pair of earley item and offset into its rhs
+  # Using the completed earley items, determine which items or implicit
+  # terminals match the given completed item and input.
+  #
+  # item: the item we're decomposing
+  # input: the input symbols
+  # states: Hash[item start] => Array[Earley::Item] completed items
+  # depth: which symbol in the item we're looking for
+  # pos: which position in the state chart we're at
+  # items: the items found so far
+  def decompose(root, input, states, depth = 0, pos = 0, items = [])
+    indent = " " * (depth * 2)
+    debug "#{indent}decompose #{root} at depth #{depth} pos #{pos} #{items.inspect}"
 
-      found = while (node = stack.pop)
-        break false
+    if root.kind_of?(Earley::Terminal) || pos == root.to
+      debug "#{indent}  matched: #{items.inspect}"
+      return items
+    end
 
+    if depth >= root.rhs.length || pos > root.to
+      debug "#{indent}  no match"
+      return nil
+    end
+
+    symbol = root.rhs[depth]
+
+    # check for terminals
+    if input[pos] == symbol
+      debug "#{indent}  input matches at #{pos}"
+      terminal = Earley::Terminal.new(symbol, pos)
+      if (path = decompose(root, input, states, depth + 1, pos + 1, items))
+        debug "#{indent}  found with terminal #{symbol}: #{path.inspect}"
+        return path
       end
-
-      paths << path if found
     end
 
+    # look at rules that might match
+    to_search = states.fetch(pos, []).select { |item| item.lhs == symbol }
+    to_search.each do |match|
+      if (path = decompose(root, input, states, depth + 1, pos + match.length, items + [match]))
+        debug "#{indent}  #{match} -> match"
+        return path
+      else
+        debug "#{indent}  #{match} -> nil"
+      end
+    end
 
-
-
-    paths
+    nil
   end
 end
 
@@ -217,39 +247,12 @@ def part2(input)
     hash[k] ||= []
     hash[k] << v.scan(ATOMS).to_a
   end
-  # for this parser, everything can be considered a terminal *and possibly* a nonterminal too
-  terminals = rules.to_a.flatten.uniq # .reject { |t| rules.keys.include?(t) }
   medicine = input.strip.scan(ATOMS).to_a
 
-  parser = Earley.new(rules, debug: true)
-  parser.parse(medicine, "e")
-
-
-  # one at a time...
-
-  # puts "reconstructing tree..."
-  # now that we have a completed parse (hopefully!)
-  # stack = [[top]]
-
-  # while path = stack.pop
-  #   item = path.last
-  #   puts "#{path.map(&:to_s).join(", ")} : #{item}"
-  #   left = item.start
-  #   right = item.pos
-  #   item.rhs.each do |sub|
-  #     puts "  looking for completed #{sub} starting at #{left} ending at or before #{right}"
-  #     left.upto(right) do |i|
-  #       state_set[i].each do |candidate|
-  #         puts "    #{candidate} #{candidate.complete?}"
-  #       end
-  #     end
-  #   end
-  # end
-
-  # path, cost = search.path(start: medicine, goal: "e")
-  # puts "path:"
-  # puts path&.join("\n")
-  # cost
+  parser = Earley.new(rules)
+  tree = parser.parse(medicine, "e")
+  puts tree.pretty_inspect
+  tree.flatten.length
 end
 
 part 1
@@ -257,6 +260,7 @@ with :part1
 try ex1, expect: 4
 try puzzle_input
 
+# simple test
 ex2 = <<-EX
 e => H
 e => O
@@ -292,7 +296,7 @@ e => XX
 X => XX
 X => X(X)
 
-XX
+XX(X(X,XX,X)X)XX
 EX
 
 ex5 = <<-EX
@@ -328,10 +332,12 @@ EX
 
 part 2
 with :part2
+debug!
 try ex2, expect: 3
 try ex3, expect: 6
-try ex4, expect: 1234
+try ex4, expect: 9
 # try ex5, expect: 38
 # try ex6, expect: 195
 # try ex7, expect: 195
-# try puzzle_input
+no_debug!
+try puzzle_input
