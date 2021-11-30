2ebb2941a2dc2cbdba7ff5269517ffd6da80a40d
Nathan Witmer <nathan@zerowidth.com>
2021-11-29 20:33:54 -0700

    2018 day 12 part 2
    Analytical approach, kind of: looked at output for a few thousand generations
    and saw a pattern.

--- 2018/12.rb
+++ 2018/12.rb
@@ -1,6 +1,6 @@
 require_relative "../toolkit"
 
-def part1(input, generations:)
+def part1(input, generations: 20)
   state, rules = input.split("\n\n")
 
   rules = rules.lines.map do |line|
@@ -8,15 +8,38 @@ def part1(input, generations:)
     [left.chars, right]
   end.to_h
 
-  pots = {}
-  # state.split(": ", 2).last.chars.each.with_index do |char, i|
-  #   pots[i] = char
-  # end
+  state = state.split(": ", 2).last.chars
+  added = 0
+  debug "#{0}: #{state.join}"
+  state, added = iterate(rules, state, generations, added)
+  state.map.with_index { |c, i| c == "#" ? (i - added) : 0 }.sum
+end
+
+def part2(input, generations:)
+  state, rules = input.split("\n\n")
+
+  rules = rules.lines.map do |line|
+    left, right = line.chomp.split(" => ")
+    [left.chars, right]
+  end.to_h
 
   state = state.split(": ", 2).last.chars
   added = 0
   debug "#{0}: #{state.join}"
 
+  # assume the pattern settles down after 5 blocks of 100 iterations
+  increments = 5.times.map do
+    state, added = iterate(rules, state, 100, added)
+    state.map.with_index { |c, i| c == "#" ? (i - added) : 0 }.sum
+  end
+
+  per_hundred = increments.each_cons(2).map { |a, b| b - a }.uniq
+  raise "dunno" if per_hundred.length > 1
+
+  (generations / 100) * per_hundred.first
+end
+
+def iterate(rules, state, generations, added)
   generations.times_with_progress do |generation|
     # ensure there are enough empty pots
     while state.take(5) != %w(. . . . .)
@@ -32,7 +55,7 @@ def part1(input, generations:)
     debug "#{generation + 1}: #{state.join}" if debug?
   end
 
-  state.map.with_progress(total: state.length).with_index { |c, i| c == "#" ? (i - added) : 0 }.sum
+  [state, added]
 end
 
 ex1 = <<-EX
@@ -62,5 +85,7 @@ no_debug!
 try puzzle_input
 
 part 2
-with :part1, generations: 50_000_000_000
+# debug!
+with :part2, generations: 1000
+with :part2, generations: 50_000_000_000
 try puzzle_input
