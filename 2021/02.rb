704efd2b8f1f3a4fa4416d008ba2f5ee4be80d16
Nathan Witmer <nathan@zerowidth.com>
2021-12-01 22:04:58 -0700

    2021 day 2

---
+++ 2021/02.rb
@@ -0,0 +1,61 @@
+require_relative "../toolkit"
+
+def part1(input)
+  pos = depth = 0
+  input.lines.each do |line|
+    num = line.numbers.first
+    case line
+    when /forward/
+      pos += num
+    when /up/
+      depth -= num
+    when /down/
+      depth += num
+    end
+  end
+  input
+
+  depth * pos
+end
+
+def part2(input)
+  pos = depth = aim =0
+  input.lines.each do |line|
+    num = line.numbers.first
+    case line
+    when /forward/
+      pos += num
+      depth += aim * num
+    when /up/
+      aim -= num
+    when /down/
+      aim += num
+    end
+  end
+  input
+
+  depth * pos
+end
+
+ex1 = <<EX
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+EX
+
+part 1
+with :part1
+debug!
+try ex1, expect: 150
+no_debug!
+try puzzle_input
+
+part 2
+with :part2
+debug!
+try ex1, expect: 900
+no_debug!
+try puzzle_input
