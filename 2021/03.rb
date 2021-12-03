1615b0c24670d9175d12b764084868e5ec5af0eb
Nathan Witmer <nathan@zerowidth.com>
2021-12-02 22:56:12 -0700

    2021 day 3 much cleaner now

--- 2021/03.rb
+++ 2021/03.rb
@@ -1,68 +1,30 @@
 require_relative "../toolkit"
 
 def part1(input)
-  counts = Hash.new(0)
-  input.lines.map(&:chomp).each do |line|
-    line.each_char.with_index { |c, i| counts[i] += 1 if c == "1" }
-  end
-  debug input.lines.length
-  debug counts
-  gamma = counts.sort_by(&:first).map { |_i, v| v > input.lines.length / 2 ? "1" : "0" }.join
-  epsilon = gamma.tr("01", "10")
-  debug "gamma #{gamma}"
-  debug "eps #{epsilon}"
-  gamma.to_i(2) * epsilon.to_i(2)
+  input = input.lines(chomp: true).map(&:chars)
+  by_digit = input.shift.zip(*input).map(&:tally)
+  debug "counts by digit: #{by_digit}"
+  epsilon = by_digit.map { |counts| counts.max_by(&:last).first }.join.to_i(2)
+  gamma = by_digit.map { |counts| counts.min_by(&:last).first }.join.to_i(2)
+  epsilon * gamma
 end
 
-def get_oxygen(lines)
-  debug "#{lines.length} total lines"
-  length = lines.first.length
-  (0...length).each do |i|
-    ones = lines.map { |l| l[i] }.count("1")
-    zeros = lines.length - ones
-    debug "i #{i}: #{ones} ones #{zeros} zeros"
-    if ones >= zeros
-      debug "keeping 1"
-      lines = lines.select { |line| line[i] == "1" }
-    else
-      debug "keeping 0"
-      lines = lines.select { |line| line[i] == "0" }
-    end
+def filter_lines(lines)
+  (0...lines.first.length).each do |i|
+    counts = lines.map { |l| l[i] }.tally
+    to_find = yield counts
+    debug "i #{i}: #{counts}, keeping #{to_find}"
+    lines = lines.select { |l| l[i] == to_find }
     break if lines.length == 1
-
-    debug "#{lines.length} #{lines}"
   end
+  debug lines.first.join
   lines.first.join.to_i(2)
-end
-
-def get_scrubber(lines)
-  debug "#{lines.length} total lines"
-  length = lines.first.length
-  (0...length).each do |i|
-    ones = lines.map { |l| l[i] }.count("1")
-    zeros = lines.length - ones
-    debug "i #{i}: #{ones} ones #{zeros} zeros"
-    if ones >= zeros
-      debug "keeping 0"
-      lines = lines.select { |line| line[i] == "0" }
-    else
-      debug "keeping 1"
-      lines = lines.select { |line| line[i] == "1" }
-    end
-    break if lines.length == 1
-
-    debug "#{lines.length} #{lines}"
-  end
-  lines.first.join.to_i(2)
-
 end
 
 def part2(input)
-  lines = input.lines.map(&:chomp).map(&:chars)
-
-  oxygen = get_oxygen(lines)
-  scrubber = get_scrubber(lines)
-
+  lines = input.lines(chomp: true).map(&:chars)
+  oxygen = filter_lines(lines) { |c| c["1"] >= c["0"] ? "1" : "0" }
+  scrubber = filter_lines(lines) { |c| c["0"] <= c["1"] ? "0" : "1" }
   oxygen * scrubber
 end
 
