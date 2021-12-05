a4bf71a03b463668a9ec7fa72b6a1c0a99f53a01
Nathan Witmer <nathan@zerowidth.com>
2021-12-04 22:32:01 -0700

    2021 day 5: refactor to use common helper

--- 2021/05.rb
+++ 2021/05.rb
@@ -1,51 +1,33 @@
 require_relative "../toolkit"
 
-def part1(input)
-  lines = input.lines_of(:numbers)
-  # find points where more than one line overlaps
-
+def points(lines)
   # brute force: all points for all lines
   points = Hash.of_array
   lines.each.with_index do |(x1, y1, x2, y2), i|
-    next unless x1 == x2 || y1 == y2
-
-    if x1 == x2
-      [y1, y2].min.upto([y1, y2].max) { |y| points[[x1, y]] << i }
-    else
-      [x1, x2].min.upto([x1, x2].max) { |x| points[[x, y1]] << i }
+    debug "line #{i} from #{x1} #{y1} to #{x2} #{y2}"
+    dx = x2 <=> x1
+    dy = y2 <=> y1
+    x, y = x1, y1
+    points[[x, y]] << i
+    loop do
+      x += dx
+      y += dy
+      points[[x, y]] << i
+      break if x == x2 && y == y2
     end
   end
-  points.count { |p, vs| vs.length > 1 }
+  points
+end
+
+def part1(input)
+  lines = input.lines_of(:numbers)
+  lines.select! { |x1, y1, x2, y2| x1 == x2 || y1 == y2 }
+  points(lines).values.count { |v| v.length > 1 }
 end
 
 def part2(input)
   lines = input.lines_of(:numbers)
-  # find points where more than one line overlaps
-
-  # brute force: all points for all lines
-  points = Hash.of_array
-  lines.each.with_index do |(x1, y1, x2, y2), i|
-    if x1 == x2
-      [y1, y2].min.upto([y1, y2].max) { |y| points[[x1, y]] << i }
-    elsif y1 == y2
-      [x1, x2].min.upto([x1, x2].max) { |x| points[[x, y1]] << i }
-    else
-      # diagonals
-      debug "line #{i} from #{x1} #{y1} to #{x2} #{y2}"
-      dx = x1 > x2 ? -1 : 1
-      dy = y1 > y2 ? -1 : 1
-      x, y = x1, y1
-      points[[x, y]] << i
-      loop do
-        debug "  #{x} #{y}"
-        x += dx
-        y += dy
-        points[[x, y]] << i
-        break if x == x2 && y == y2
-      end
-    end
-  end
-  points.count { |_p, vs| vs.length > 1 }
+  points(lines).values.count { |v| v.length > 1 }
 end
 
 ex1 = <<EX
@@ -72,5 +54,5 @@ part 2
 with :part2
 debug!
 try ex1, 12
-# no_debug!
+no_debug!
 try puzzle_input # not 18115
