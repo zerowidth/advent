a2a5481a4f770204cb945115d8260d2a3e1c1b67
Nathan Witmer <nathan@zerowidth.com>
2021-12-15 22:29:06 -0700

    2021 day 16 part 1

--- 2021/16.rb
+++ 2021/16.rb
@@ -2,18 +2,22 @@ require_relative "../toolkit"
 
 def part1(input)
   bits = input.chars.map { |c| c.to_i(16).to_s(2).rjust(4, "0") }.join.digits
-  parse(bits)
+  versions = parse_versions(bits)
+  debug "versions: #{versions}"
+  versions.sum
 end
 
 def decode(bits)
   bits.join.to_i(2)
 end
 
-def parse(bits)
+def parse_versions(bits)
   version = decode(bits.shift(3))
   debug "version #{version}"
   type = decode(bits.shift(3))
   debug "type: #{type}"
+
+  versions = [version]
 
   case type
   when 4 # literal value
@@ -37,7 +41,7 @@ def parse(bits)
       subpacket = bits.shift(length)
       until subpacket.empty?
         debug "parsing subpacket"
-        parse(subpacket)
+        versions.concat parse_versions(subpacket)
       end
     when 1
       # next 11 bits are number of sub-packets contained
@@ -45,13 +49,13 @@ def parse(bits)
       debug "subpacket count: #{count}"
       count.times do |n|
         debug "parsing subpacket #{n}"
-        parse(bits)
+        versions.concat parse_versions(bits)
       end
     else
       raise "wtf length type id #{length_type_id}"
     end
   end
-  nil
+  versions
 end
 
 def part2(input)
@@ -61,13 +65,15 @@ end
 ex1 = "D2FE28"
 ex2 = "38006F45291200"
 ex3 = "EE00D40C823060"
+ex4 = "8A004A801A8002F478"
 
 part 1
 with :part1
 debug!
-try ex1, nil
-try ex2, nil
-try ex3, nil
+try ex1, 6
+try ex2, 9
+try ex3, 14
+try ex4, 16
 no_debug!
 try puzzle_input
 
