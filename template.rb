ea1d117489f2443c2ae38ded29a70b31430372f9
Nathan Witmer <nathan@zerowidth.com>
2021-12-18 21:56:55 -0700

    update template

--- template.rb
+++ template.rb
@@ -2,24 +2,26 @@ require_relative "../toolkit"
 
 def part1(input)
   input.lines
-  nil
 end
 
 def part2(input)
   input.lines
-  nil
 end
+
+ex1 = <<EX
+
+EX
 
 part 1
 with :part1
 debug!
-# try ex1, expect: nil
+try ex1, nil
 no_debug!
 try puzzle_input
 
 part 2
 with :part2
 debug!
-# try ex1, expect: nil
+try ex1, nil
 no_debug!
 try puzzle_input
