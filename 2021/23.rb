a394b4bc6fa9d628f685770fbcb6a74c7dadebde
Nathan Witmer <nathan@zerowidth.com>
2021-12-30 15:51:39 -0700

    2021 day 23 part 2, faster with simpler representation

--- 2021/23.rb
+++ 2021/23.rb
@@ -7,6 +7,111 @@ def cost_for(amphipod, distance)
   distance * (10 ** PODS.index(amphipod))
 end
 
+class AmphipodSearch
+  attr_reader :distances
+  attr_reader :intermediates
+
+  # internal:
+  attr_reader :hlen # hallway length
+  attr_reader :rdepth # room depth
+
+  # operates on single-array states:
+  # [0..6 hallway, 7..14 rooms, 15 cost]
+  #
+  # hallway_coords: list of x coords for hallway positions
+  # room_coords: x coords for rooms
+  # rooms: array of arrays of starting amphipod positions
+  #
+  # pre-calculates costs and intermediate nodes for each hallway-to-room and
+  # room-to-hallway transition, using lists of integer offsets into a state array
+  def initialize(hallway_coords, room_coords, rooms)
+    @distances = Hash.of_hash
+    @intermediates = Hash.of_hash
+
+    @rdepth = rooms.first.length
+    @hlen = hallway_coords.length
+    hallway_coords.each.with_index do |hx, hi|
+      room_coords.each.with_index do |rx, ri|
+        # intermediates between hallway and room:
+        is = hx.to(rx).map { |x| x == hx ? nil : hallway_coords.index(x) }.compact
+        rdepth.times do |r|
+          i = hlen + (ri * rdepth) + r
+          dist = (hx - rx).abs + 1 + r
+          is << (i - 1) if r > 0 # mutating but it's ok
+          distances[hi][i] = dist
+          distances[i][hi] = dist
+          intermediates[hi][i] = is.dup
+          intermediates[i][hi] = is.dup
+        end
+      end
+    end
+  end
+
+  def neighbors(state)
+    ns = []
+
+    # find hallway-to-room transitions:
+    0.upto(hlen - 1) do |hi|
+      next unless (pod = state[hi])
+
+      # what room is this pod is supposed to go to?
+      room = hlen + (PODS.index(pod) * rdepth)
+
+      # does the room have only the right kind of amphipod in it?
+      next unless state[room, rdepth].all? { |p| p.nil? || p == pod }
+
+      # get the deepest empty space:
+      ri = room + state[room, rdepth].rindex(nil)
+      next unless clear?(state, hi, ri)
+
+      new_state = state.dup
+      new_state[hi] = nil
+      new_state[ri] = pod
+      new_state[-1] += cost(pod, distances[hi][ri])
+      ns << new_state
+    end
+
+    # find room-to-hallway transitions:
+    (rdepth * 4).times do |ri|
+      room = ri / rdepth
+      rd = ri % rdepth
+      ri += hlen
+
+      next unless (pod = state[ri])
+      next if PODS.index(pod) == room && state[ri, rdepth - rd].all? { |p| p.nil? || p == pod }
+
+      # find all available hallway locations to move to
+      hlen.times do |hi|
+        next unless state[hi].nil? && clear?(state, ri, hi)
+
+        new_state = state.dup
+        new_state[hi] = pod
+        new_state[ri] = nil
+        new_state[-1] += cost(pod, distances[ri][hi])
+        ns << new_state
+      end
+    end
+
+    ns
+  end
+
+  def clear?(state, from, to)
+    intermediates[from][to].all? { |i| state[i].nil? }
+  end
+
+  def cost(amphipod, distance)
+    distance * (10 ** PODS.index(amphipod))
+  end
+
+  def draw(state)
+    hallway = state[0, hlen].map { |h| h || "." }
+    rooms = state[hlen, rdepth * 4].each_slice(rdepth).map do |room|
+      room.map { |r| r || "." }.join
+    end
+    "#{hallway} #{rooms.join(" ")} #{state.last}"
+  end
+end
+
 class State
   attr_reader :hallway, :rooms, :cost, :hallway_positions, :room_positions
 
@@ -116,46 +221,70 @@ class State
   end
 end
 
+def solve(hlxs, rxs, rooms)
+  pods = AmphipodSearch.new(hlxs, rxs, rooms)
+  start = ([nil] * pods.hlen) + rooms.flatten + [0]
+  finish = PODS.flat_map { |p| [p] * rooms.first.length }
+  debug { "start: #{start}" }
+  debug { "finish: #{finish}" }
+
+  search = GraphSearch.new do |config|
+    config.debug = debug?
+    config.cost = ->(from, to) { to.last - from.last }
+    # config.heuristic = lambda do |neighbor, _goal|
+    #    (4 - (neighbor.rooms & finish).size) * 1000
+    # end
+    config.neighbors = ->(state) { pods.neighbors(state) }
+    # config.break_if = ->(cost, _best) { cost > 13000 }
+    # config.each_step = lambda do |start, current, came_from, cost_so_far|
+      # debug current.to_s
+      # debug { "start: #{show start}" }
+      # if current.hallway.index("C") == 6
+      #   print ("-" * 80).colorize(:magenta)
+        # gets
+      # end
+    # end
+    # config.each_step = ->(*) { print "> "; gets }
+  end
+
+  path = search.path(start: start) do |state|
+    state[pods.hlen, pods.rdepth * 4] == finish
+  end
+  debug { "path:\n#{path && path.first.map { |p| pods.draw(p) }.join("\n")}" }
+  path
+end
+
 def part1(input)
   rooms = input.lines[2, 2]
   # room x coords:
   rxs = rooms.first.indices(/[A-Z]/).to_a
   # hallway x coords:
   hlxs = input.lines[1].indices(".").to_a - rxs
-
+  # arrays of amphipods
   rooms = rooms.map { |r| r.scan(/[A-Z]/) }.transpose
-  hallway = Array.new(hlxs.last, nil)
 
-  start = State.new(hallway, rooms, 0, hlxs, rxs)
-  finish = PODS.map { |p| [p] * rooms.first.length }
-
-  search = GraphSearch.new do |config|
-    config.debug = debug?
-    config.cost = ->(from, to) { to.cost - from.cost }
-    config.heuristic = lambda do |neighbor, _goal|
-       (4 - (neighbor.rooms & finish).size) * 1000
-    end
-    config.neighbors = ->(state) { state.neighbors }
-    config.break_if = ->(cost, _best) { cost > 13000 }
-    # config.each_step = lambda do |start, current, came_from, cost_so_far|
-      # debug current.to_s
-      # debug { "start: #{show start}" }
-      # if current.hallway.index("C") == 6
-      #   print ("-" * 80).colorize(:magenta)
-      #   gets
-      # end
-    # end
-  end
-
-  path = search.path(start: start) do |state|
-    state.rooms == finish
-  end
-  debug { "path:\n#{path && path.first.map(&:to_s).join("\n")}" }
+  path = solve(hlxs, rxs, rooms)
   path&.last
 end
 
-def part2(input)
-  input.lines
+part2_extra = <<EX
+#D#C#B#A#
+#D#B#A#C#
+EX
+
+def part2(input, extra)
+  extra = Input.new(extra).lines
+  rooms = input.lines[2, 2]
+  rooms = [rooms[0]] + extra + [rooms[1]]
+  # room x coords:
+  rxs = rooms.first.indices(/[A-Z]/).to_a
+  # hallway x coords:
+  hlxs = input.lines[1].indices(".").to_a - rxs
+  # arrays of amphipods
+  rooms = rooms.map { |r| r.scan(/[A-Z]/) }.transpose
+
+  path = solve(hlxs, rxs, rooms)
+  path&.last
 end
 
 ex1 = <<EX
@@ -174,8 +303,8 @@ no_debug!
 try puzzle_input
 
 part 2
-with :part2
+with :part2, part2_extra
 debug!
-try ex1, nil
+try ex1, 44169
 no_debug!
 try puzzle_input
