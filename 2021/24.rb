9b613c39d72459d5bd1dbcde1d60fedddd7b1662
Nathan Witmer <nathan@zerowidth.com>
2021-12-31 15:55:36 -0700

    solve 2021 day 24 analytically

--- 2021/24.rb
+++ 2021/24.rb
@@ -339,72 +339,54 @@ def valid?(input)
   z
 end
 
-def part1(input)
-  debug { "z = 0" }
-  input.split("inp w").drop(1).map { |s| Input.new(s.strip).lines }.each_with_index do |lines, i|
+def get_ranges(input)
+  sections = input.split("inp w").drop(1)
+  ranges = Array.new(sections.length)
+  adds = []
+
+  # debug { "z = 0" }
+  sections.map { |s| Input.new(s.strip).lines }.each_with_index do |lines, i|
     case lines[3]
     when "div z 1" # multiplying z
-      # find out how much we're adding
       adding = lines[14].signed_numbers.first
-      debug { "z = (z * 26) + input[#{i}] + #{adding}" }
+      # debug { "z = (z * 26) + input[#{i}] + #{adding}" }
+      adds << [adding, i]
     when "div z 26" # reducing z
       subtracting = lines[4].signed_numbers.first
-      debug do
-        sub = subtracting < 0 ? "- #{subtracting.abs}" : "+ #{subtracting}"
-        "x = (z % 26) #{sub}\nz /= 26\nreturn nil unless x == input[#{i}]"
-      end
+      # debug { "x = (z % 26) - #{subtracting.abs}\nz /= 26\nreturn nil unless x == input[#{i}]" }
+
+      # we know:
+      #   z = (whatever) * 26 + some input + some addition
+      #   x = (some input + some addition) - subtraction
+      # given:
+      #   z = (z * 26) + input[1] + 10
+      #   x = (z % 26) - 11
+      #   return nil unless x == input[12]
+      # we know that input[1] + 10 - 11 == input[12], or: input[12] = input[1] - 1
+      add, addi = adds.pop
+      diff = add + subtracting
+      xrange = (1..9).map { |r| r + diff } & (1..9).to_a
+      arange = (1..9).map { |r| r - diff } & (1..9).to_a
+      debug { "  input[#{i}]: #{xrange}" }
+      debug { "  input[#{addi}]: #{arange}" }
+      ranges[i] = xrange
+      ranges[addi] = arange
     else
       raise "wtf: #{lines[3]}"
     end
-    debug
+    # debug
   end
-  debug { "z" }
+  # debug { "z" }
+
+  ranges
 end
 
-def part1_manual(input)
-  code = translate(input)
-  puts "-" * 10
-  puts code.join("\n")
-  puts "-" * 10
-  eval code.join("\n")
+def part1(input)
+  get_ranges(input).map(&:last).map(&:to_s).join
+end
 
-  ranges = [
-    (1..8).to_a, # 0, propagate to 13
-    (2..9).to_a, # 1, propagate to 12
-    [9], # 2, propagate to 3
-    (1..3).to_a, # 4, propagate to 11
-    (8..9).to_a, # 5, propagate to 8
-    (1..4).to_a, # 6 propagate to 7
-    (7..9).to_a, # 9, propagate to 10
-  ]
-
-  bar = progress_bar(title: "validating...")
-  found = []
-  ranges[0].product(*ranges[1..]) do |seq|
-    expanded = [
-      seq[0], # 0
-      seq[1], # 1
-      seq[2], # 2
-      seq[2] - 8, # 3
-      seq[3], # 4
-      seq[4], # 5
-      seq[5], # 6
-      seq[5] + 5, # 7
-      seq[4] - 7, # 8
-      seq[6], # 9
-      seq[6] - 6, # 10
-      seq[3], # 11
-      seq[1] - 1, # 12
-      seq[0], # 13
-    ]
-    bar.advance
-    puts "#{expanded} : #{monad(expanded)}"
-    next unless monad(expanded) == 0
-
-    puts expanded.to_s
-    found << expanded
-  end
-  found
+def part2(input)
+  get_ranges(input).map(&:first).map(&:to_s).join
 end
 
 def translate(input)
@@ -434,17 +416,13 @@ def translate(input)
   code << "end"
 end
 
-def part2(input)
-  input.lines
-end
-
 part 1
 with :part1
 debug!
 # no_debug!
 try puzzle_input
 
-# part 2
-# with :part2
+part 2
+with :part2
 # no_debug!
-# try puzzle_input
+try puzzle_input
