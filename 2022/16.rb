8ed8fb1043d4f2fe0358fde4e8856b52ddacee8a
Nathan Witmer <nathan@zerowidth.com>
2022-12-16 15:49:42 -0700

    2022 day 16 part 2 too slow still

--- 2022/16.rb
+++ 2022/16.rb
@@ -92,43 +92,63 @@ def part1(input)
   best&.total_flow
 end
 
-Location = Struct.new(:position, :time_remaining, :path) do
+Location = Struct.new(:position, :time_remaining) do
   def move_to(new_position, cost)
-    self.class.new(new_position, time_remaining - cost, path + [new_position])
+    # self.class.new(new_position, time_remaining - cost, path + [new_position])
+    self.class.new(new_position, time_remaining - cost)
   end
+
+  def to_s
+    "#{position}:#{time_remaining}"
+  end
+  alias_method :inspect, :to_s
 end
 
-P2State = Struct.new(:locations, :open_valves, :total_flow)
+P2State = Struct.new(:locations, :open_valves, :total_flow) do
+  def id
+    "#{locations.map(&:to_s).join(";")};#{open_valves.to_a.sort.join(";")}"
+  end
+end
 
 def part2(input)
   edges, rates, costs, to_open = preprocess(input)
 
-  stack = [P2State.new([Location.new("AA", 30, [])], Set.new, 0)]
+  stack = [P2State.new([Location.new("AA", 26), Location.new("AA", 26)], Set.new, 0)]
   best = nil
   n = 0
+  skipped = 0
+  seen = Set.new
   while (node = stack.shift)
+    node_id = node.id
+    if seen.include?(node_id)
+      skipped += 1
+      next
+    end
+    seen << node_id
     n += 1
-    debug "n #{n} stack #{stack.size} current #{node}" if n % 10000 == 0
+    puts "n #{n} stack #{stack.size} skipped #{skipped} current #{node}" if n % 10000 == 0
     # debug "node: #{node}"
 
-    still_closed = to_open - node.open_valves
-    if node.locations.all? { |loc| loc.time_remaining <= 0 } || still_closed.empty?
+    if to_open.length == node.open_valves.length || node.locations.all? { |loc| loc.time_remaining <= 0 }
       if node.total_flow > (best&.total_flow || 0)
-        debug "best so far: #{node}"
+        puts "best so far: #{node}"
         best = node
       end
       next
     end
 
-    node.locations.each.with_index do |loc, i|
+    still_closed = to_open - node.open_valves
+    locations = node.locations # .sort_by(&:time_remaining).reverse
+    locations.each.with_index do |loc, i|
       next if loc.time_remaining <= 0
 
       still_closed.each do |next_location|
+        next if node.locations.any? { |l| l.position == next_location }
         cost = costs.fetch(loc.position).fetch(next_location) + 1 # move and open
         time_remaining = loc.time_remaining - cost
-        locations = node.locations.dup
-        locations[i] = loc.move_to(next_location, cost)
-        stack << P2State.new(locations, node.open_valves + [next_location], node.total_flow + rates[next_location] * time_remaining)
+        next_locations = locations.dup
+        next_locations[i] = loc.move_to(next_location, cost)
+        stack << P2State.new(next_locations, node.open_valves + [next_location], node.total_flow + rates[next_location] * time_remaining)
       end
     end
   end
@@ -161,7 +181,6 @@ try puzzle_input
 part 2
 with :part2
 debug!
-# try ex1, 1707
-try ex1, 1651
+try ex1, 1707
 no_debug!
 try puzzle_input
